dfeb3d85184a3c803c13b0f8351ded8a81d84dea
NoOne045-dev <naxokairo045@gmail.com>
2026-09-11 20:20:10 +0530

    Remove: LinkPreviewOptions import

--- Plugins/Sequence.py
+++ Plugins/Sequence.py
@@ -7,7 +7,7 @@ import html as html_lib
 from datetime import datetime
 
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, LinkPreviewOptions
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.enums import ParseMode, ChatAction, ChatMemberStatus
 from pyrogram.raw import types as raw_types, functions as raw_functions
@@ -1322,7 +1322,7 @@ async def leaderboard_cmd(client: Client, message: Message):
         text, kb = await build_leaderboard_view(message.from_user.id, "alltime")
         await handle_floodwait(
             message.reply_text, text, reply_markup=kb, parse_mode=ParseMode.HTML,
-            link_preview_options=LinkPreviewOptions(is_disabled=True)
+            disable_web_page_preview=True
         )
     except Exception as e:
         logger.error(f"Leaderboard error: {e}", exc_info=True)
